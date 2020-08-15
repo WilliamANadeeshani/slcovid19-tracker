8ab33c80e44e9214730b9ebeb8a0615817e65bca
Nadeeshani William <nadeeshanijayarathna93@gmail.com>
2020-08-16 00:33:40 +0530

    changed mock services to https -> heroku deployment

--- src/service/constants.js
+++ src/service/constants.js
@@ -1,2 +1,2 @@
 export const CURRENT_DATA_API = 'https://hpb.health.gov.lk/api/get-current-statistical';
-export const VISITED_PLACES_API = 'http://demo4245099.mockable.io/visitedPublicPlaces';
+export const VISITED_PLACES_API = 'https://demo4245099.mockable.io/visitedPublicPlaces';
